d840e5813c220f5f0f308dd20e309b9958688d3c
elangovana <aparnaelangovan@hotmail.com>
2014-10-18 15:36:56 +1100

    Commit Fix previous

---
+++ AE.MachineLearning.NeuralNet.Core/ICostFunction.cs
@@ -0,0 +1,19 @@
+﻿namespace AE.MachineLearning.NeuralNet.Core
+{
+    public interface ICostFunction
+    {
+        /// <summary>
+        ///     Cost (Error per training data)
+        /// </summary>
+        /// <param name="target">expected value of output</param>
+        /// <param name="actual">actual Value or output</param>
+        /// <returns>Cost</returns>
+        double Cost(double target, double actual);
+
+        double Cost(double[] target, double[] actual);
+
+        double DerivativeCostWrtOutput(double target, double actual);
+
+        double DerivativeCostWrtOutput(double[] target, double[] actual);
+    }
+}
